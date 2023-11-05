0c3531703346a270aba365c40018fab0ef300492
Mondesir <ridelntsoumou@gmail.com>
2023-11-05 11:45:23 +0100

    change package.json

--- filesystem-server.js
+++ filesystem-server.js
@@ -765,25 +765,26 @@ function FileManagerDirectoryContent(req, res, filepath, searchFilterPath) {
         var cwd = {};
         replaceRequestParams(req, res);
         fs.stat(filepath, function (err, stats) {
-            if (!stats) {
+            if (stats) {
+                cwd.name = path.basename(filepath);
+                cwd.size = getSize(stats.size);
+                cwd.isFile = stats.isFile();
+                cwd.dateModified = stats.ctime;
+                cwd.dateCreated = stats.mtime;
+                cwd.type = path.extname(filepath);
+                if (searchFilterPath) {
+                    cwd.filterPath = searchFilterPath;
+                } else {
+                    cwd.filterPath = req.body.data.length > 0 ? req.body.path : "";
+                }
+                cwd.permission = getPathPermission(req.path, cwd.isFile, (req.body.path == "/") ? "" : cwd.name, filepath, contentRootPath, cwd.filterPath);
+                if (fs.lstatSync(filepath).isFile()) {
+                    cwd.hasChild = false;
+                    resolve(cwd);
+                }
+            }else{
                 res.redirect(req.originalUrl);
                 return;
-            }
-            cwd.name = path.basename(filepath);
-            cwd.size = getSize(stats.size);
-            cwd.isFile = stats.isFile();
-            cwd.dateModified = stats.ctime;
-            cwd.dateCreated = stats.mtime;
-            cwd.type = path.extname(filepath);
-            if (searchFilterPath) {
-                cwd.filterPath = searchFilterPath;
-            } else {
-                cwd.filterPath = req.body.data.length > 0 ? req.body.path : "";
-            }
-            cwd.permission = getPathPermission(req.path, cwd.isFile, (req.body.path == "/") ? "" : cwd.name, filepath, contentRootPath, cwd.filterPath);
-            if (fs.lstatSync(filepath).isFile()) {
-                cwd.hasChild = false;
-                resolve(cwd);
             }
         });
         if (fs.lstatSync(filepath).isDirectory()) {
